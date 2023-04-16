bfa7cd7c19a23314ea0956f1f68a9870ee221257
Kirill <scerep529@gmail.com>
2023-04-17 00:24:01 +0600

    validate commit

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,45 @@
-import express, { Express } from "express";
-import { Server } from "http";
-import { LoggerService } from "./logger/logger.service";
-import { UserController } from "./users/users.controller";
-import { ExeptionFilter } from "./errors/exeption.filter";
+import express, { Express } from 'express';
+import { Server } from 'http';
+import { UserController } from './users/users.controller';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { ILogger } from './logger/logger.interface';
+import { inject, injectable } from 'inversify';
+import { TYPES } from './types';
+import { json } from 'body-parser';
+import 'reflect-metadata';
 
+@injectable()
 export class App {
-    app: Express;
-    server: Server;
-    port: number;
-    logger: LoggerService;
-    userController: UserController;
-    exeptionFilter: ExeptionFilter;
+	app: Express;
+	server: Server;
+	port: number;
 
-    constructor(logger: LoggerService, userController: UserController, exeptionFilter: ExeptionFilter) {
-        this.app = express();
-        this.port = 8000;
-        this.logger = logger;
-        this.userController = userController;
-        this.exeptionFilter = exeptionFilter;
-    }
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private userController: UserController,
+		@inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
 
-    useRoutes() {
-        this.app.use('/users', this.userController.router);
-    }
+	useMiddleware(): void {
+		this.app.use(json());
+	}
 
-    useExeptionFilters() {
-        this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
-    }
+	useRoutes(): void {
+		this.app.use('/users', this.userController.router);
+	}
 
-    public async init() {
-        this.useRoutes();
-        this.useExeptionFilters();
-        this.app.listen(this.port);
-        this.logger.log(`Сервер запущен на http://localhost:${this.port}/`);
-    }
+	useExeptionFilters(): void {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+	}
+
+	public async init(): Promise<void> {
+		this.useMiddleware();
+		this.useRoutes();
+		this.useExeptionFilters();
+		this.app.listen(this.port);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}/`);
+	}
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,47 @@
-import { BaseController } from "../common/base.controller";
-import { HTTPError } from "../errors/http-error.class";
-import { LoggerService } from "../logger/logger.service";
+import { inject, injectable } from 'inversify';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { ILogger } from '../logger/logger.interface';
 import { Request, Response, NextFunction } from 'express';
+import { TYPES } from '../types';
+import { IUserController } from './users.controller.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { UserService } from './users.service';
+import 'reflect-metadata';
+import { ValidateMiddleware } from '../common/validate.middleware';
 
-export class UserController extends BaseController {
-    constructor(logger: LoggerService) {
-        super(logger);
-        this.bindRoutes([
-            { path: "/register", method: 'post', func: this.register },
-            { path: "/login", method: 'post', func: this.login },
-        ]);
-    }
+@injectable()
+export class UserController extends BaseController implements IUserController {
+	constructor(
+		@inject(TYPES.ILogger) private loggerService: ILogger,
+		@inject(TYPES.UserService) private userService: UserService,
+	) {
+		super(loggerService);
+		this.bindRoutes([
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
+			},
+			{ path: '/login', method: 'post', func: this.login },
+		]);
+	}
 
-    login(req: Request, res: Response, next: NextFunction) {
-        // this.ok(res, 'login');
-        next(new HTTPError(401, 'user not login', 'login'));
-    }
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		next(new HTTPError(401, 'user not login', 'login'));
+	}
 
-    register(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'register');
-    }
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.createUser(body);
+		if (!result) {
+			return next(new HTTPError(422, 'Такой пользователь уже существует'));
+		}
+		this.ok(res, { email: result.email });
+	}
 }
